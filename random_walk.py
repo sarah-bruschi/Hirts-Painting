b6d90f1f295ca70f7fed049418e3046305f43503
sarah-bruschi <sarah.bruschi@gmail.com>
2022-01-25 09:30:26 -0500

    created a random walk with color changes with each rotation

---
+++ random_walk.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+import random
+
+
+tim = Turtle()
+
+colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "Wheat", "SlateGray", "SeaGreen"]
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+
+for _ in range(200):
+    tim.color(random.choice(colors))
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+
+screen = Screen()
+screen.exitonclick()
