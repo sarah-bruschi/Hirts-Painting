6b079d8965d311006bd1ca3daec1f822fd2b583f
sarah-bruschi <sarah.bruschi@gmail.com>
2022-01-25 09:47:09 -0500

    update random_walk to have truly random color selection while drawing

--- random_walk.py
+++ random_walk.py
@@ -1,18 +1,27 @@
-from turtle import Turtle, Screen
+import turtle as t 
 import random
 
 
-tim = Turtle()
+tim = t.Turtle()
+t.colormode(255)
 
-colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "Wheat", "SlateGray", "SeaGreen"]
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r,g,b)
+    return random_color 
+
 directions = [0, 90, 180, 270]
 tim.pensize(15)
 tim.speed("fastest")
 
 for _ in range(200):
-    tim.color(random.choice(colors))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
 
-screen = Screen()
+
+
+screen = t.screen()
 screen.exitonclick()
